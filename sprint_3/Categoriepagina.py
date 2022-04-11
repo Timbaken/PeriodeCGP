c8cfbb07e73fabb57138803b9d84899aebfe4349
Dexieschool <dexter.fijnvandraat@student.hu.nl>
2022-04-11 10:28:02 +0200

    categorie pagina engine fix

--- sprint_3/Categoriepagina.py
+++ sprint_3/Categoriepagina.py
@@ -2,7 +2,7 @@ import psycopg2
 import random
 
 try:
-    conn = psycopg2.connect("dbname=test user=postgres host=localhost password=#Starwars04")
+    conn = psycopg2.connect("dbname=Opdracht2AI user=postgres host=localhost password=appelflap")
 except:
     print("I am unable to connect to the database")
 
@@ -10,17 +10,17 @@ except:
 cur = conn.cursor()
 
 def categorypagina(category):
-        select_statment="select orderd_products_id.orderd,count(*) as total " \
-                        "from orderd_products_id" \
-                        "left join product on orderd_products_id.orderd = product.product_id" \
-                        "where product.category=%s" \
-                        "group by orderd" \
-                        "order by total desc" \
-                        "limit 5"
+        select_statment="select orderd_products_id.orderd,count(*) as total from orderd_products_id left join product on orderd_products_id.orderd = product.product_id where product.category=%s group by orderd order by total desc limit 5"
 
         #voert de sql query uit
         cur.execute(select_statment,(category,))
 
         ophaler=cur.fetchall()
-        return ophaler
+        lst1 = []
 
+        for i in ophaler:
+            lst1.append(i[0])
+
+        return lst1
+
+print(categorypagina('Huishouden'))
